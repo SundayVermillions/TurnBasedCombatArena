1e31fddc21d2028a7644a238c8e87ce6cb9bb576
nairdash <s.darshannair@gmail.com>
2026-04-07 18:46:37 +0800

    place the Potion heal amount to static constant so it can heal without creating the potion object

--- src/main/java/tbca/item/Potion.java
+++ src/main/java/tbca/item/Potion.java
@@ -4,10 +4,11 @@ import tbca.engine.GameState;
 import tbca.engine.action.results.SpecialSkillResults;
 
 public class Potion implements Item{
+    public static final int HEAL_AMOUNT = 100;
     @Override
     public SpecialSkillResults use(Combatant user, GameState gameState, int targetIndex){
-        int healAmount = 100;
-        int newHp = Math.min(user.getCurrHp() + healAmount, user.getMaxHp());
+
+        int newHp = Math.min(user.getCurrHp() + HEAL_AMOUNT, user.getMaxHp());
         user.setHp(newHp);
         return null;
     }
