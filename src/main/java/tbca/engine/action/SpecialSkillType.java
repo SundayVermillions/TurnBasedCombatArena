edff5da41cb0ed91ee10ced697b811d97cf984b4
nairdash <s.darshannair@gmail.com>
2026-04-07 18:47:49 +0800

    added the description for the wolf

--- src/main/java/tbca/engine/action/SpecialSkillType.java
+++ src/main/java/tbca/engine/action/SpecialSkillType.java
@@ -3,7 +3,7 @@ package tbca.engine.action;
 public enum SpecialSkillType {
     SHIELD_BASH("Shield Bash", "Deal BasicAttack damage to selected enemy. Selected enemy is unable to take actions for the current turn and the next turn."),
     ARCANE_BLAST("Arcane Blast", "Deal BasicAttack damage to all enemies currently in combat. Each enemy defeated by Arcane Blast adds 10 to the Wizard's Attack, lasting until end of the level."),
-    WOLF_FURY("Wolf Fury", "Need to add description."),
+    WOLF_FURY("Wolf Fury", "The wolf prepares a vicious attack, doubling its attack power for 1 turn."),
     NONE("None", "");
 
     private final String displayName;
